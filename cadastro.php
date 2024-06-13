1482168abd0b8bb2ce6901b8978a7af7d1daf4bb
Virgilio <36864103+tvirgilio@users.noreply.github.com>
2024-06-12 23:47:43 -0300

    Update cadastro.php
    Ajuste no arquivo index, adicionando os novos links

--- cadastro.php
+++ cadastro.php
@@ -1,56 +1,33 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-	<table>
-		<form action="cd_recebe.php" method="POST">
-			<tr>
-				<td>Nome:</td>
-				<td><input type="text" name="nome"></td>
-			</tr>
-			<tr>
-				<td>Email:</td>
-				<td><input type="email" name="email"></td>
-			</tr>
-			<tr>
-				<td>Telefone:</td>
-				<td><input type="text" name="telefone"></td>
-			</tr>
-			<tr>
-				<td>Curso:</td>
-				<td>
-					<select name="curso">
-						<option>&nbsp;</option>
-						<option value="adm">Administracao</option>
-						<option value="ds">Desenvolvimento de Sistemas</option>
-						<option value="log">Logistica</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>CPF:</td>
-				<td><input type="text" name="cpf"></td>
-			</tr>
-			<tr>
-				<td>
-					Data de Nascimento:
-				</td>
-				<td>
-					<input type="date" name="data">
-				</td>
-			</tr>
-			<tr>
-				<tr>
-					<td colspan="2">
-						<input type="submit" value="Cadastrar">
-					</td>
-				</tr>
-			</tr>
-		</form>
-	</table>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+        <style type="text/css">
+            body{
+                font-family: verdana;
+                color: #000;
+                font-size: 24px;
+            }
+            a{
+                text-decoration: none;
+                color: #000;
+            }
+        </style>
+    </head>
+    <body>
+        <a href="cadastro.php">
+            CADASTRO
+         </a><br> 
+        <a href="consulta_alunos.php"> 
+            CONSULTA
+        </a><br>
+        <a href="modificar_alunos.php">  
+            MODIFICAR
+        </a><br>
+        <a href="excluir_aluno.php">
+            EXCLUIR
+        </a><br>
+    </body>
 </html>
