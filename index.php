c3e0c90d2b3e491abfc1d6882b9354f29d14989c
Virgilio <36864103+tvirgilio@users.noreply.github.com>
2024-06-19 19:42:25 -0300

    Update index.php
    Ajuste de links no arquivo index.php

--- index.php
+++ index.php
@@ -10,14 +10,24 @@
                 color: #000;
                 font-size: 24px;
             }
+            a{
+                text-decoration: none;
+                color: #000;
+            }
         </style>
     </head>
     <body>
         <a href="cadastro.php">
             CADASTRO
-         </a><br>   
-        CONSULTA
-        MODIFICAR
-        EXCLUIR
+         </a><br> 
+        <a href="consulta_alunos.php"> 
+            CONSULTA
+        </a><br>
+        <a href="modificar_alunos.php">  
+            MODIFICAR
+        </a><br>
+        <a href="excluir_aluno.php">
+            EXCLUIR
+        </a><br>
     </body>
 </html>
